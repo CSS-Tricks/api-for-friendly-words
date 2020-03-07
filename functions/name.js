815d7582556fbae3d6934198c68c7ae3e055f555
Chris Coyier <chriscoyier@gmail.com>
2020-03-07 11:48:18 -0800

    words api

--- functions/name.js
+++ functions/name.js
@@ -1,6 +1,18 @@
+const friendlyWords = require("friendly-words");
+
 exports.handler = function(event, context, callback) {
+  const { predicates, objects } = friendlyWords;
+  const numberOfPredicates = predicates.length;
+  const numbersOfObjects = objects.length;
+
+  const randomPredicate =
+    predicates[Math.floor(Math.random() * numberOfPredicates)];
+  const randomObject = objects[Math.floor(Math.random() * numbersOfObjects)];
+
+  const output = `${randomPredicate}-${randomObject}`;
+
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: output
   });
 };
